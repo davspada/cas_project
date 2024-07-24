b57e5746101aa70e7e622daefb907ab95e510069
Davide Spada <dav.spada@outlook.com>
2024-07-24 11:05:03 +0200

    Setup android app with kotlin and 3 fragment navigation

---
+++ app/app/src/main/java/com/davspada/geofencingnotification/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.davspada.geofencingnotification.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
