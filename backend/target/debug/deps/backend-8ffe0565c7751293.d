36f409463d0370df2bd84c97edbd3153acc02c6a
L0P0P <emanuelegrasso@live.com>
2024-07-25 22:35:44 +0200

    Added backend setup

---
+++ backend/target/debug/deps/backend-8ffe0565c7751293.d
@@ -0,0 +1,5 @@
+/home/emanuele/Universita/cas_project/backend/target/debug/deps/backend-8ffe0565c7751293: src/main.rs
+
+/home/emanuele/Universita/cas_project/backend/target/debug/deps/backend-8ffe0565c7751293.d: src/main.rs
+
+src/main.rs:
