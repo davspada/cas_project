ce45f4ea12eb0be205cbdfc349656a8fbb896a74
Davide Spada <dav.spada@outlook.com>
2024-12-30 16:22:38 +0100

    add notification setup

---
+++ mobile/services/notifications.tsx
@@ -0,0 +1,46 @@
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications'
+import { Platform } from 'react-native';
+import * as Device from 'expo-device';
+
+export async function registerForPushNotificationsAsync() {
+    let token;
+  
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+  
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      // Learn more about projectId:
+      // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+  
+      if (Constants.easConfig?.projectId) {
+        token = (
+          await Notifications.getExpoPushTokenAsync({
+            projectId: '8c314219-fc15-4b29-aa77-68050aabf66f'//Constants.easConfig.projectId, // you can hard code project id if you dont want to use expo Constants
+          })
+        ).data;
+        console.log(token);
+      }
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+  
+    return token;
+  }
